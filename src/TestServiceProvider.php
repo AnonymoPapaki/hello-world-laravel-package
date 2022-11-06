6c8139a085f5d5717b4d359719209d91287c2e8b
eugene <nokanoki@gmail.com>
2022-11-06 15:21:57 +0200

    sadas

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -14,6 +14,6 @@ class TestServiceProvider extends ServiceProvider
     }
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
     }
 }
