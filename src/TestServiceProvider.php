13231631da8371f2cc8f338c709a38ae738d3dad
eugene <nokanoki@gmail.com>
2022-11-06 14:57:31 +0200

    asdas

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -14,6 +14,6 @@ class TestServiceProvider extends ServiceProvider
     }
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 }
